ae967354610cb8c7f665ccd02245e065c7899125
kohnish <kohnish@gmx.com>
2022-07-20 10:30:05 +0200

    typo

--- native/app/test-vim9-nox11.cxx
+++ native/app/test-vim9-nox11.cxx
@@ -2,7 +2,7 @@
 #include "vim9_nox11_env.h"
 #include <gtest/gtest.h>
 
-TEST(handle_msg, handle_msg_1) {
+TEST(handle_msg_1, handle_msg_1) {
     handle_msg("+1 test/test /v");
     handle_msg("test/test /v");
     handle_msg("test/test");
